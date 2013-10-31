8dc10ccaee2669c0c94b6c676937d5618a8feb25
Rebecca Sliter <sliterrm@gmail.com>
2013-10-31 10:35:43 -0500

    WORKS! for all articles in json file

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,24 +1,36 @@
 'use strict';
 
 function VerbosCtrl($scope, $http) {
+  var indexDeInfinitivo = 0;
+
   self.getVerbo = function(){
     $http.get('verbos/verbos.json').success(function(data) {
-      var randomVerbIndex = Math.floor(Math.random() * data.length)
-      $scope.verbo = data[randomVerbIndex];
+      var randomVerbIndex = Math.floor(Math.random() * data.length);
+      var verboArticulos = data[randomVerbIndex];
+
+
+      var articulos = Object.keys(verboArticulos)
+      articulos.splice(articulos.indexOf('infinitivo'), 1);
+      var randomArticuloIndex = Math.floor(Math.random() * articulos.length);
+
+      $scope.articulo = articulos[randomArticuloIndex];
+      $scope.verbo = verboArticulos[$scope.articulo];
+      $scope.infinitivo = verboArticulos["infinitivo"];
     });
-  }
+  };
 
   self.getVerbo();
 
   $scope.probeConjugado = function() {
-    if ($scope.verbo.yo == $scope.verbo.conjugado){
-      $scope.verbo.correcto = true;
+    if ($scope.verbo == $scope.conjugado){
+      $scope.correcto = true;
       self.getVerbo();
     } else {
-      $scope.verbo.correcto = false;
+      $scope.correcto = false;
     }
-  };
 
+    $scope.conjugado = "";
+  };
 
 }
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -17,29 +17,48 @@ describe('controllers', function(){
 
     it('should create a Verbos model with a single verb', function() {
       $httpBackend.flush();
-      expect(scope.verbo).toEqual({"infinitivo":"comer","yo":"como"});
+      expect(scope.verbo).toEqual("como");
+    });
+
+    it('should create a Verbo model', function() {
+      $httpBackend.flush();
+      expect(scope.infinitivo).toEqual("comer");
+    });
+
+    it('should create an Articulo model with a random Articulo', function() {
+      $httpBackend.flush();
+      expect(scope.articulo).not.toBe(null);
     });
 
     it('sends incorrect message if verb is conjugated incorrectly', function() {
       $httpBackend.flush();
-      scope.verbo.conjugado = "WRONG";
+      scope.verbo = "como";
+      scope.conjugado = "WRONG";
       scope.probeConjugado();
-      expect(scope.verbo.correcto).toEqual(false);
+      expect(scope.correcto).toEqual(false);
     });
 
     it('sends correct message if verb is conjugated correctly', function() {
       $httpBackend.flush();
-      scope.verbo.conjugado = "como";
+      scope.verbo = "como";
+      scope.conjugado = "como";
       $httpBackend.expectGET('verbos/verbos.json').respond([{"infinitivo":"comer","yo":"como"}]);
       scope.probeConjugado();
-      expect(scope.verbo.correcto).toEqual(true);
+      expect(scope.correcto).toEqual(true);
     });
 
     it('gets a new verb if verb is conjugated correctly', function() {
       $httpBackend.flush();
-      scope.verbo.conjugado = "como";
+      scope.verbo = "como";
+      scope.conjugado = "como";
       $httpBackend.expectGET('verbos/verbos.json').respond([{"infinitivo":"comer","yo":"como"}]);
       scope.probeConjugado();
+    });
+
+    it('clears the conjugated Model after testing for correctness', function() {
+      $httpBackend.flush();
+      scope.probeConjugado();
+      expect(scope.conjugado).toBe("");
     });
   });
 });
